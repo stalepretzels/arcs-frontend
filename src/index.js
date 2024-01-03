a9b6e3e1a4d54030b950674081c56417758f0c94
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-03 12:40:38 -0500

    Fix height bug and link error.

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
   },
   {
     path: "*", // Catch-all route used for errors
-    element: <p>This page <em>probably</em> doesn't exist. Check back later, or <a href="https://github.com/stalepretzels/arcs-frontend/issues" target="_blank">file an issue</a></p>
+    element: <p>This page <em>probably</em> doesn't exist. Check back later, or <a href="https://github.com/stalepretzels/arcs-frontend/issues" target="_blank" rel="noreferrer">file an issue</a></p>
   }
 ]);
 
