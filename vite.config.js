522c27e515063a70634ab68fba11f7f784a26da5
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-04 08:53:47 -0500

    add more to version

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+var read = require('read-file');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000, },
   define: {
-    __APP_VERSION__: JSON.stringify('v1.0.0'),
+    __APP_VERSION__: JSON.stringify(`${new TextDecoder("utf-8").decode(read.sync())}`),
 //    __SERVER_LOCATION__: JSON.stringify(`${process.env.SERVER_LOCATION}`),
   },
 })
