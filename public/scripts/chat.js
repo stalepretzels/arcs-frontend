73c70918849665a2c35009d453eaec03ec402db0
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-08 12:19:29 -0500

    please JUST WORK

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -33,7 +33,7 @@ class MailmanClass extends MailSystemClass {
 
         this.pkg = {
           message: this.message,
-          date: new Date(),
+          date: new Date(Date.now()),
           room: this.room,
           user: user,
           //attachment: attachment
