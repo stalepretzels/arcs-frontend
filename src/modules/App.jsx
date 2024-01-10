7fe8da113623a5a6e3748fd60074abcee253a7bc
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-10 12:06:56 -0500

    sizing

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -30,8 +30,8 @@ function Chat() {
           maxLength="2000"
         ></textarea>
 <input type="file" id="selectedFile" accept="image/*" />
-<IconButton size="2em" classes="chatFiles button" icon="folder" onClick={() => document.getElementById('selectedFile').click() }></IconButton>
-        <IconButton id="chatSubmit" classes="button" size="2em" icon="send"></IconButton>
+<IconButton size="1.6em" classes="chatFiles button" icon="folder" onClick={() => document.getElementById('selectedFile').click() }></IconButton>
+        <IconButton id="chatSubmit" classes="button" size="1.6em" icon="send"></IconButton>
       </div>
     </>
   );
