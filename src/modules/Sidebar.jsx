cf4557bc6167ab18fd252ab720176fd588faa6cf
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-10 10:27:55 -0500

    update /about icon

--- src/modules/Sidebar.jsx
+++ src/modules/Sidebar.jsx
@@ -82,7 +82,7 @@ function Sidebar() {
         </li>
         <li>
           <div id="connected" className="menuButton">
-            <ion-icon name="cloud" style={{fontSize: '32px'}}></ion-icon>
+            <ion-icon name="information-circle" style={{fontSize: '32px'}}></ion-icon>
           </div>
         </li>
       </ul>
