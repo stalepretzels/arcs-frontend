1dfd4aef6f2478c7b45361baf9288256b7363d18
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-10 11:03:14 -0500

    add sizing

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -31,7 +31,7 @@ function Chat() {
         ></textarea>
 <input type="file" id="selectedFile" accept="image/*" />
 <IconButton size="16px" classes="chatFiles button" icon="folder" onClick={() => document.getElementById('selectedFile').click() }>Browse...</IconButton>
-        <IconButton id="chatSubmit" classes="button" icon="send"></IconButton>
+        <IconButton id="chatSubmit" classes="button" size="16px" icon="send"></IconButton>
       </div>
     </>
   );
