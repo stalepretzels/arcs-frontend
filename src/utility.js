2b6ed57388b3936df731ba3846352681522c6b9c
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-30 08:32:53 -0500

    bug fix

--- src/utility.js
+++ src/utility.js
@@ -2,6 +2,7 @@ export function addDependencyScripts(depArray) {
     let scriptList = []
     depArray.forEach((elem)=>{
       let script = document.createElement('script');
+      script.type = "module"
       script.src = elem;
       scriptList.push(script);
       document.body.appendChild(script);
