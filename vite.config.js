d670ab6017a52f9b592d0164d064e448cbdc47d7
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-02-05 11:43:42 -0500

    wait no

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: { port: 3000, },
   define: {
-    __APP_VERSION__: JSON.stringify(`${new TextDecoder("utf-8").decode(read.sync('./version.txt'))}`),
+    __APP_VERSION__: JSON.stringify(`v${new TextDecoder("utf-8").decode(read.sync('./version.txt'))}`),
     __APP_CHANGELOG__: JSON.stringify(`${new TextDecoder("utf-8").decode(read.sync('./changelog.txt'))}`),
     __APP_VERSION_HANDLE__: JSON.stringify("Dragonfly")
 //    __SERVER_LOCATION__: JSON.stringify(`${process.env.SERVER_LOCATION}`),
