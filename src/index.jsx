86ec7a0ef8521be7aaf4df85a7c9372ddec096b7
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-11 09:31:56 -0400

    Remove privatization.

--- src/index.jsx
+++ src/index.jsx
@@ -43,15 +43,6 @@ const router = createBrowserRouter([
     element: <ChangelogAbout />
   },
   {
-    path: "/privatization",
-    element: <>This project is being privated on the 31st of March, 2024. I am sorry it has come to this. I have spent the better half of 2 years working on this project, and it appears it is time to rest.<br/>
-    My reasoning for doing this is simple, and it is that there are simply too many issues to handle with filtration. The amount of anti-semitic, racist, and homophobic jokes being spread among the platform I hold dear to my heart is sad to see.<br/>
-    This shutdown was caused by a group of people, which for some reason, decided to spread hate among Arcs as a joke. It isn't funny, and it never was. You can attempt to change my mind on this, but I shall not budge.<br/>
-    This is a mere privatization, and not a sunset or shutdown, so still expect to see this come back, but this is goodbye for now.<br/>
-    <br/>
-    - Clover</>
-  },
-  {
     path: "*", // Catch-all route used for errors
     element: (
       <p>
@@ -71,8 +62,6 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="privatizationNotice"><a href="/privatization">This project is being privated on the 31st of March, 2024. I am sorry it has come to this.</a></div>
-
     <Sidebar />
 
     <main className="mainContainer">
