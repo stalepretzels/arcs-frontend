05539c451cbf66752c35171ffddc46bf987309d1
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-05-20 12:44:32 -0400

    make new build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+# clone client
+git clone https://github.com/stalepretzels/arcs-client 
+cd arcs-client
+
+# build client
+
+## install rust toolchain
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+## build with webpack
+pnpm i
+pnpm run build
+
+## copy build
+mkdir ../src/client
+mv dist/* ../src/client
+
+# (debug) list files
+cd ../src/client
+ls
+cd ../../
+
+# build frontend
+pnpm i vite -g 
+vite build
